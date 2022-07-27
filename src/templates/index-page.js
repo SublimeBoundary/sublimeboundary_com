7ab6390675990385adf640942907b4881d6188d7
SublimeBoundary <info@sublimeboundary.com>
2022-07-27 03:31:17 +0100

    Change text from 'Latest stories' to 'Latest news'

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -49,7 +49,7 @@ export const IndexPageTemplate = ({
 
                   <div className="column is-12">
                     <h3 className="has-text-weight-semibold is-size-2">
-                      Latest stories
+                      Latest news
                     </h3>
                     <BlogRoll />
                     <div className="column is-12 has-text-centered">
